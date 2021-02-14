433031d0f3e6e3ff6cd200bd81867221e20a3e46
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 17:45:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function createEmployeeRecord(array)
 {
   return {
     firstName: array[0],
-    lastName: array[1],
+    familyName: array[1],
     title: array[2],
     payPerHour: array[3],
     timeInEvents: [],
